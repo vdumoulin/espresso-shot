f3ad5462c752bffb02cf8b4538095d4672abdfda
Vincent Dumoulin <vi.dumoulin@gmail.com>
2021-02-28 09:55:16 -0500

    Add Arduino source code, Python script, and Jupyter Notebook

---
+++ espresso_shot.ino
@@ -0,0 +1,331 @@
+/*
+  Code for an Arduino-based espresso shot management device for E61 group head
+  machines. Highly inspired by Howard Smith's setup (https://
+  www.home-barista.com/tips/guide-to-managing-hx-brew-temperatures-t64840.html).
+
+  The device performs the following:
+
+  - Monitors basket and group temperatures using thermistors. The group
+    thermistor is inserted in an adapter screwed into the M6 closure plug (e.g.
+    https://www.home-barista.com/espresso-machines/monitoring-brew-temperature-
+    e61-groups-t1352.html). The basket thermistor is wrapped in foil tape and
+    sandwiched between the portafilter basket and the group head.
+  - Times the shot using a tilt switch taped to the brew lever.
+  - Displays basket temperature, group temperature, and shot time on an OLED.
+  - Controls a DC fan which air cools the group head to a target temperature.
+    The target temperature is selectable with a potentiometer.
+  - Sends time and temperature logging information over serial. A companion
+    Python script listens to the serial channel and converts the information
+    into JSON files that a companion Jupyter Notebook can then read and display.
+*/
+
+#include <Adafruit_ADS1015.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
+// Number of times per second that we read temperatures.
+#define SENSING_FREQUENCY 100
+
+// Temperatures are averaged over a certain time horizon to reduce noise. By
+// default we set it to the sensing frequency so that we get an average over the
+// previous second.
+#define BUFFER_SIZE SENSING_FREQUENCY
+
+// We use the Steinhart-Hart model to characterize the relationship between
+// thermistor resistance and temperature. The coefficients A, B, and C are
+// calculated empirically on three temperature-resistance pairs using an online
+// calculator (e.g. https://www.thinksrs.com/downloads/programs/therm%20calc/
+// ntccalibrator/ntccalculator.html).
+#define BASKET_SH_A 0.7871221906e-3
+#define BASKET_SH_B 2.039769726e-4
+#define BASKET_SH_C 1.469069094e-7
+#define GROUP_SH_A 0.7871221906e-3
+#define GROUP_SH_B 2.039769726e-4
+#define GROUP_SH_C 1.469069094e-7
+
+// The basket and group thermistors and the shot timer switch are connected to
+// an ADS1115's channels 1, 2, and 3 (respectively) using a pull-up resistor
+// configuration. The known resistance values for the basket and group voltage
+// divider circuits are measured empirically. The reference voltage is measured
+// on channel 0.
+#define REFERENCE_VOLTAGE_CHANNEL 0
+#define BASKET_VOLTAGE_CHANNEL 1
+#define BASKET_KNOWN_RESISTANCE 9940.0
+#define GROUP_VOLTAGE_CHANNEL 2
+#define GROUP_KNOWN_RESISTANCE 9940.0
+#define TILT_CHANNEL 3
+
+// The cooling fan attempts to keep the group head at the target temperature.
+// A potentiometer allows to adjust that target temperature.
+#define FAN_PIN 13
+#define TARGET_TEMPERATURE_PIN A0
+#define TARGET_TEMPERATURE_MIN 88.0
+#define TARGET_TEMPERATURE_MAX 98.0
+
+// Number of times per second that we refresh the display.
+#define DISPLAY_FREQUENCY 4
+
+// Number of milliseconds to display the target temperature for when it changes.
+#define TARGET_DISPLAY_TIME 1000
+
+// Compute sensing and display periods to simplify implementation.
+constexpr unsigned short SENSING_PERIOD = 1000.0 / SENSING_FREQUENCY;
+constexpr unsigned short DISPLAY_PERIOD = 1000.0 / DISPLAY_FREQUENCY;
+
+// We use an ADS1115 for data acquisition and an OLED screen to display
+// information.
+Adafruit_ADS1115 ads1115;
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0);
+
+// We use circular buffers to compute basket and group temperature averages over
+// a certain time horizon determined by SENSOR_FREQUENCY and BUFFER_SIZE.
+int latest_buffer_index;
+float basket_temperature_buffer[BUFFER_SIZE];
+float group_temperature_buffer[BUFFER_SIZE];
+
+// Temperature buffer averages.
+float current_basket_temperature;
+float current_group_temperature;
+
+// Selected target group temperature.
+float target_group_temperature;
+
+// The start time is used with millis() to determine the elapsed time. When the
+// machine is not running we display the previous shot time that was recorded
+// into elapsed_time.
+unsigned long start_time;
+float elapsed_time;
+
+// Whether the machine is currently running.
+bool running;
+
+// Historical device state.
+bool previously_running;
+unsigned long last_temperature_measurement;
+unsigned long last_display_refresh;
+unsigned long last_target_change;
+
+void setup() {
+  // Initialize devices and pins.
+  Serial.begin(9600);
+  ads1115.begin();
+  u8g2.begin();
+  pinMode(FAN_PIN, OUTPUT);
+
+  // Initialize running state.
+  running = false;
+  previously_running = false;
+
+  // Initialize temperatures.
+  target_group_temperature = read_target_temperature();
+  current_basket_temperature = read_basket_temperature();
+  current_group_temperature = read_group_temperature();
+  
+  latest_buffer_index = 0;
+  
+  for (int i = 0; i < BUFFER_SIZE; ++i) {
+    basket_temperature_buffer[i] = current_basket_temperature;
+    group_temperature_buffer[i] = current_group_temperature;
+  }
+  
+  // Initialize time.
+  start_time = millis();
+  last_display_refresh = start_time;
+  last_target_change = start_time;
+  elapsed_time = 0.0;
+}
+
+void loop() {
+  // Determine the running state of the machine, as determined by the tilt
+  // switch.
+  running = read_voltage(TILT_CHANNEL) > 3.0;
+
+  unsigned long current_time = millis();
+
+  // When a state transition from "stopped" to "running" occurs, reset the
+  // elapsed time and start the timer.
+  if (running && !previously_running) {
+    start_time = current_time;
+    elapsed_time = 0.0;
+  }
+
+  // When the machine is running or has just stopped, update the timer.
+  if (running || previously_running) {
+    elapsed_time = (current_time - start_time) / 1000.0;
+  }
+
+  // Measure temperatures when the sensing period has passed.
+  if (current_time > last_temperature_measurement + SENSING_PERIOD) {
+    update_temperatures();
+    last_temperature_measurement = current_time;
+
+    // Send time and temperature information over serial.
+    String message = (
+        String(elapsed_time, 2) + "," +
+        String(basket_temperature_buffer[latest_buffer_index], 5) + "," +
+        String(group_temperature_buffer[latest_buffer_index], 5) + ","
+    );
+    if (running) {
+      message += previously_running ? "RUNNING" : "START";
+    } else {
+      message += previously_running ? "STOP" : "STOPPED";
+    }
+    Serial.println(message);
+  }
+
+  // Read the target group temperature set by the user. If it has changed,
+  // update the target group temperature and reset the timer for displaying
+  // that information instead of the group temperature.
+  float new_target_temperature = read_target_temperature();
+  // Target temperatures are rounded to the nearest multiple of 0.5, so to be
+  // extra careful about determining equality on floats we double the numbers
+  // and cast them as int before doing the comparison.
+  if ((int) 2 * new_target_temperature != (int) 2 * target_group_temperature) {
+    target_group_temperature = new_target_temperature;
+    last_target_change = current_time;
+  }
+
+  // We cool the group head until it reaches the target temperature. We could
+  // eventually dampen the temperature swings by implementing PID control, but
+  // for now this is good enough.
+  digitalWrite(
+      FAN_PIN,
+      current_group_temperature > target_group_temperature ?  HIGH : LOW
+  );
+
+  // Refresh the display when the refresh period has passed.
+  if (current_time > last_display_refresh + DISPLAY_PERIOD) {
+    refresh_display();
+    last_display_refresh = current_time;
+  }
+
+  previously_running = running;
+}
+
+// Reads the target group temperature set by the user on the potentiometer.
+// Target temperatures are rounded to the nearest multiple of 0.5.
+float read_target_temperature() {
+  // We want to round to the nearest multiple of 0.5, so instead we map to
+  // double the (integer) range and divide by 2.
+  int double_target_temperature = map(
+      analogRead(TARGET_TEMPERATURE_PIN), 0, 1023, 180, 200
+  );
+  return double_target_temperature / 2.0;
+}
+
+// Reads and returns the voltage at the specified ADC channel.
+float read_voltage(uint8_t channel) {
+  return 0.0001875 * ads1115.readADC_SingleEnded(channel);
+}
+
+// Reads and returns a thermistor's temperature at the specified ADC channel
+// given the specified known resistance and Steinhart-Hard coefficients.
+float read_temperature(uint8_t channel, float known_resistance,
+                       float sh_a, float sh_b, float sh_c) {
+  // Infer the resistance from the voltage divider circuit.
+  float reference_voltage = read_voltage(REFERENCE_VOLTAGE_CHANNEL);
+  float voltage = read_voltage(channel);
+  float resistance = known_resistance / ((reference_voltage / voltage) - 1.0);
+
+  // We use the Steinhart-Hart model to infer the temperature (in degrees
+  // Kelvin) from the thermistor's resistance.
+  float inverse_temperature_kelvin = (
+    sh_a + sh_b * log(resistance) + sh_c * pow(log(resistance), 3)
+  );
+
+  return 1.0 / inverse_temperature_kelvin - 273.15;
+}
+
+// Reads the basket temperature from its corresponding thermistor. Wraps
+// read_temperature for convenience.
+float read_basket_temperature() {
+  return read_temperature(BASKET_VOLTAGE_CHANNEL, BASKET_KNOWN_RESISTANCE,
+                          BASKET_SH_A, BASKET_SH_B, BASKET_SH_C);
+}
+
+// Reads the group temperature from its corresponding thermistor. Wraps
+// read_temperature for convenience.
+float read_group_temperature() {
+  return read_temperature(GROUP_VOLTAGE_CHANNEL, GROUP_KNOWN_RESISTANCE,
+                          GROUP_SH_A, GROUP_SH_B, GROUP_SH_C);
+}
+
+// Updates the basket and group temperature buffers and recomputes the average
+// basket and group temperatures.
+void update_temperatures() {
+  // Update temperature buffers.
+  latest_buffer_index = (latest_buffer_index + 1) % BUFFER_SIZE;
+  basket_temperature_buffer[latest_buffer_index] = read_basket_temperature();
+  group_temperature_buffer[latest_buffer_index] = read_group_temperature();
+
+  // Compute temperature averages. It would be more efficient to remove the
+  // the temperature overwritten in the buffer from the average and add the new
+  // temperature to the average, but since thermistors can be disconnected from
+  // the system the running average can be contaminated by NaNs. We use the
+  // inefficient but safe approach instead.
+  current_basket_temperature = 0.0;
+  current_group_temperature = 0.0;
+  for (int i = 0; i < BUFFER_SIZE; ++i) {
+    current_basket_temperature += basket_temperature_buffer[i];
+    current_group_temperature += group_temperature_buffer[i];
+  }
+  current_basket_temperature /= BUFFER_SIZE;
+  current_group_temperature /= BUFFER_SIZE;
+}
+
+// Refreshes the OLED screen using current basket / group temperature and
+// elapsed time.
+void refresh_display() {
+  // We use dtostrf for string formatting using variables of type float. See
+  // https://arduino.stackexchange.com/questions/53712/creating-formatted-
+  // string-including-floats-in-arduino-compatible-c. The character buffer size
+  // for the float to string conversion is 7 (WXYZ.D, plus the null termination
+  // character); we don't expect basket or group temperatures to be below
+  // -999.9C or above 9999.9C. We use a second character buffer to include the
+  // unit of measurement.
+  char number_buffer[7];
+  char buffer[14];
+
+  // If the target group temperature changed recently, display it instead of the
+  // group temperature.
+  bool display_target = millis() <= last_target_change + TARGET_DISPLAY_TIME;
+
+  u8g2.firstPage();
+  do {
+    float time = min(3600.0, elapsed_time);
+
+    u8g2.setFont(u8g2_font_helvR10_tr);
+    u8g2.setFontMode(0);
+    u8g2.setDrawColor(1);
+
+    // Draw header.
+    u8g2.drawStr(0, 11, display_target ? "Target" : "Group");
+    u8g2.drawStr(128 - u8g2.getStrWidth("Basket") - 1, 11, "Basket");
+    u8g2.drawLine(0, 13, 127, 13);
+
+    // Display temperatures.
+    float temperature = display_target ? target_group_temperature :
+                                         current_group_temperature;
+    dtostrf(temperature, 5, 1, number_buffer);
+    snprintf(buffer, sizeof(buffer), "%sC",
+             isnan(temperature) ? "---" : number_buffer);
+    u8g2.drawStr(0, 30, buffer);
+
+    dtostrf(current_basket_temperature, 5, 1, number_buffer);
+    snprintf(buffer, sizeof(buffer), "%sC",
+             isnan(current_basket_temperature) ? "---" : number_buffer);
+    u8g2.drawStr(128 - u8g2.getStrWidth(buffer) - 1, 30, buffer);
+
+    // Display time.
+    u8g2.drawBox(0, 40, 128, 24);
+    dtostrf(elapsed_time, 5, 1, number_buffer);
+    snprintf(buffer, sizeof(buffer), "%02d:%02d.%1d",
+             int(time) / 60, int(time) % 60, int(time * 10) % 10);
+
+    u8g2.setFont(u8g2_font_helvR18_tn);
+    u8g2.setFontMode(1);
+    u8g2.setDrawColor(2);
+
+    u8g2.drawStr((128 - u8g2.getStrWidth(buffer)) / 2, 61, buffer);
+  } while ( u8g2.nextPage() );
+}
---
+++ espresso_shot.py
@@ -0,0 +1,238 @@
+"""Script to run the espresso shot management device.
+
+The script builds and uploads the Arduino sketch to the device, then starts
+listening to serial communication on the upload port and records measurement
+series to JSON files.
+
+Example usage:
+
+    $ python espresso_shot.py --fqbn <FQBN> -p <UPLOAD PORT>
+
+The space key toggles between saving measurement series to disk and simply
+displaying data on screen.
+"""
+import argparse
+import collections
+import curses
+import datetime
+import functools
+import json
+import subprocess
+import time
+
+import numpy as np
+import pytz
+import serial
+
+
+def compile_and_upload(fqbn, port):
+  """Compiles the Arduino sketch and uploads it to the device.
+
+  Args:
+    fbqn: str, fully qualified board name.
+    port: str, upload port.
+  """
+  subprocess.run(['arduino-cli', 'compile', '--fqbn', fqbn, 'espresso_shot'])
+  subprocess.run(['arduino-cli', 'upload', '-p', port, '--fqbn', fqbn,
+                  'espresso_shot'])
+
+
+def find_port_if_not_specified(fqbn, port):
+  """Finds an upload port if it's left unspecified.
+
+  If `port` is None, then uses `arduino-cli board list` to find all boards
+  connected to the computer with the specified fully qualified board name
+  and sets `port` to that of the first board found.
+
+  Args:
+    fbqn: str, fully qualified board name.
+    port: str or None, upload port.
+
+  Returns:
+    port: str, the upload port.
+  
+  Raises:
+    RuntimeError, if `port` is None and no board with the specified fully
+    qualified board name is connected to the computer.
+  """
+  process = subprocess.Popen(
+      ['arduino-cli', 'board', 'list', '--format', 'json'],
+      stdout=subprocess.PIPE)
+
+  devices = json.loads(process.communicate()[0].decode('utf-8'))
+  for device in devices:
+    if 'boards' in device and any(board['FQBN'] == fqbn
+                                  for board in device['boards']):
+      port = port or device['address']
+      break
+
+  if port is None:
+    raise RuntimeError('no port specified and no board with the specified '
+                       'FQBN was found.')
+
+  return port
+
+
+class MockSerial:
+  """Mock serial port used to test the interface when no device is available.
+
+  We simulate alternating between pulling a shot for 30 seconds and letting the
+  machine idle for 30 seconds, but we have time run twice as fast for
+  convenience.
+  """
+
+  def __init__(self, **kwargs):
+    self._time = 0
+    self._period = 30
+    self._running = True
+
+  def readline(self):
+    # One simulated second lasts half a real-time second.
+    time.sleep(0.5)
+
+    # Sample random basket and group temperatures.
+    basket_temperature = np.random.normal(loc=92.0, scale=0.5)
+    group_temperature = np.random.normal(loc=92.0, scale=0.5)
+
+    # The device displays the previous shot's time when the machine is idle.
+    elapsed_time = self._time if self._running else self._period
+
+    # Determine the device's state.
+    if self._running:
+      # The state is 'START' at the first measurement of a shot, and 'RUNNING'
+      # afterwards.
+      state = 'START' if self._time == 0 else 'RUNNING'
+    else:
+      # The first measurement at the end of the shot has state 'STOP', and
+      # subsequent measurements have state 'STOPPED'.
+      state = ('STOP' if (self._time == self._period and self._running) else
+               'STOPPED')
+
+    # Advance simulated time by one second, and reset to zero after 30 seconds
+    # has passed.
+    self._time = (self._time + 1) % (self._period + 1)
+
+    # Switch between "pulling a shot" and "idle" at every cycle.
+    if self._time == 0:
+      self._running = not self._running
+
+    return  '{},{},{},{}'.format(
+        elapsed_time, basket_temperature, group_temperature, state
+    ).encode('utf-8')
+
+
+def main_loop(stdscr, port, simulate):
+  """Runs the main loop.
+
+  Args:
+    stdscr: curses window object.
+    port: str, upload port.
+    simulate: bool, whether to simulate a connected device.
+  """
+  serial_class = MockSerial if simulate else serial.Serial
+  serial_port = serial_class(port=port, baudrate=9600)
+
+  # We average temperatures over the previous 100 measurements.
+  basket_temperatures = collections.deque(maxlen=100)
+  group_temperatures = collections.deque(maxlen=100)
+
+  curses.curs_set(0)
+  stdscr.nodelay(True)
+  stdscr.clear()
+
+  record_mode = False
+
+  while True:
+    # Read serial one line at a time.
+    line = serial_port.readline().decode('utf-8').strip().split(',')
+    # Data lines are formatted as
+    #   'elapsed_time,group_temperature,basket_temperature,state'.
+    elapsed_time, basket_temperature, group_temperature = map(float, line[:3])
+    state = line[3]
+    basket_temperatures.append(basket_temperature)
+    group_temperatures.append(group_temperature)
+
+    # The space key toggles the recording mode.
+    try:
+      key = stdscr.getkey()
+    except:
+      key = None
+    if key == ' ':
+      record_mode = not record_mode
+
+    # Update the terminal display
+    stdscr.clear()
+    num_rows, num_cols = stdscr.getmaxyx()
+    section_width = num_cols // 4
+
+    stdscr.addstr(0, 0, 'Elapsed time', curses.A_BOLD)
+    stdscr.addstr(1, 0, '{:.2f}'.format(elapsed_time))
+
+    stdscr.addstr(0, section_width, 'Group temperature', curses.A_BOLD)
+    stdscr.addstr(1, section_width, '{:.3f}'.format(group_temperature))
+
+    stdscr.addstr(0, 2 * section_width, 'Basket temperature', curses.A_BOLD)
+    stdscr.addstr(1, 2 * section_width, '{:.3f}'.format(basket_temperature))
+
+    stdscr.addstr(0, 3 * section_width, 'State', curses.A_BOLD)
+    stdscr.addstr(1, 3 * section_width, state)
+
+    record_mode_string = 'Recording' if record_mode else 'Not recording'
+    stdscr.addstr(num_rows - 1, num_cols - len(record_mode_string) - 1,
+                  record_mode_string)
+    stdscr.refresh()
+
+    # A new measurement series begins with the state "START".
+    if state == 'START':
+      # We will write the measurement series to a JSON file with the current
+      # date and time as its name.
+      file_path = 'data/{}.json'.format(''.join(
+          datetime.datetime.now().isoformat('-', timespec='seconds').split(':')
+      ))
+      # The shot data to be serialized to JSON.
+      shot_data = {
+        'posix time': time.time(),
+        'description': "",
+        'time': [],
+        'basket_temperature': [],
+        'group_temperature': [],
+      }
+    # A measurement series ends with the state "STOP".
+    elif state == 'STOP':
+      # When the measurement series ends, we serialize it to a JSON file.
+      if not simulate:
+        with open(file_path, 'w') as f:
+          json.dump(shot_data, f)
+    # When running, we record shot data.
+    elif state == 'RUNNING':
+      shot_data['time'].append(elapsed_time)
+      shot_data['basket_temperature'].append(basket_temperature)
+      shot_data['group_temperature'].append(group_temperature)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(
+      description='Run the espresso shot management device.')
+  parser.add_argument(
+      '--fqbn', type=str, default='arduino:avr:uno',
+      help='Fully Qualified Board Name.')
+  parser.add_argument(
+      '-p', dest='port', type=str, default=None,
+      help='Upload port, e.g.: COM10 or /dev/ttyACM0')
+  parser.add_argument(
+      '--simulate', action='store_true',
+      help='Simulate a connected Arduino device.')
+  args = parser.parse_args()
+
+  fqbn = args.fqbn
+  simulate = args.simulate
+  port = None if simulate else find_port_if_not_specified(fqbn, args.port)
+
+  if not simulate:
+    compile_and_upload(fqbn=fqbn, port=port)
+  
+  curses.wrapper(functools.partial(
+      main_loop,
+      port=port,
+      simulate=simulate,
+  ))
